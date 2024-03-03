4862c5be65fb9b064683be021c961313f2694d57
Sunil Hanamshetty <64653515+shetty456@users.noreply.github.com>
2024-03-03 10:26:02 +0530

    added static files

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "restaurant"
+    "restaurant",
 ]
 
 MIDDLEWARE = [
@@ -116,7 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"  # Ensure it ends with a slash
+STATIC_ROOT = os.path.join(BASE_DIR, "static")  # Corrected path
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
